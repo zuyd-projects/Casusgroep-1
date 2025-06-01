8422e2679bd831bd716799b7fd0e6997a7764542
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2025-06-01 15:04:43 +0200

    chore(backend): updated .gitignore file to exclude build artifacts

--- backend/ERPNumber1/ERPNumber1/Program.cs
+++ backend/ERPNumber1/ERPNumber1/Program.cs
@@ -10,6 +10,10 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddScoped<IWeatherService, WeatherService>();
 
+// Add something like this to use the DB from the docker-compose.yml file
+// builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
