7a4ad4328abb517d8222d441c3e70f24492f36d0
IHasFingas <91128974+IHasFingas@users.noreply.github.com>
2025-06-06 13:53:29 +0200

    feat(class): Added Order class

---
+++ backend/ERPNumber1/ERPNumber1/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+
+namespace ERPNumber1.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int RoundId { get; set; }
+        public int DeliveryId { get; set; }
+        public int UserId { get; set; }
+        public char MotorType { get; set; }
+        public int Quantity { get; set; }
+        public string Signature { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<Product> Products { get; set; }
+        public Order(int Id, int RoundId, int DeliveryId, int UserId, char MotorType, int Quantity, string Signature, DateTime OrderDate, List<Product> Products)
+        {
+            
+        }
+    }
+}
