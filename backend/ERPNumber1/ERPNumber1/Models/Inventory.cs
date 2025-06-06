58402930c12e2ff1a9a352ba30a89a88243b4693
IHasFingas <91128974+IHasFingas@users.noreply.github.com>
2025-06-06 16:35:49 +0200

    feat(AppDB): Added the code to AppDBContext for Delivery, Invetory and Order Classes. Also Fixxed some stuff in classes.

--- backend/ERPNumber1/ERPNumber1/Models/Inventory.cs
+++ backend/ERPNumber1/ERPNumber1/Models/Inventory.cs
@@ -7,11 +7,18 @@ namespace ERPNumber1.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int Quantity { get; set; }
-        public List<Material> Materials { get; set; }
+        public List<Product> Products { get; set; }
+        public User User { get; set; }
+        public Round Round { get; set; }
 
-        public Inventory(int Id, string Name, int Quantity, List<Material> Materials)
+        public Inventory(int id, string name, int quantity, List<Product> products, User user, Round round)
         {
-            
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            Products = products;
+            User = user;
+            Round = round;
         }
     }
 }
