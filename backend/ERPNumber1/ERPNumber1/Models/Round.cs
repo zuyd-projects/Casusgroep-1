ffe67bd8627f64c53dd2377a826557ca716e24ae
Noa Heutz <56248103+Nah-Nova@users.noreply.github.com>
2025-06-06 13:45:02 +0200

    feat(api): add constructors

---
+++ backend/ERPNumber1/ERPNumber1/Models/Round.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ERPNumber1.Models
+{
+    public class Round
+    {
+        public int Id { get; set; }
+        public int SimulationId { get; set; }
+        public int RoundNumber { get; set; }
+
+        // Navigation
+        public required Simulation Simulation { get; set; }
+        
+        // Default constructor for EF Core
+        public Round() 
+        {
+        }
+        
+        // Convenience constructor
+        public Round(Simulation simulation, int roundNumber)
+        {
+            Simulation = simulation;
+            SimulationId = simulation.Id;
+            RoundNumber = roundNumber;
+        }
+    }
+}
