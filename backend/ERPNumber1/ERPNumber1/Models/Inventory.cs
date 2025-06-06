0b802a8e08370409a4b48c71b49ac4a0ac7ea7b0
IHasFingas <91128974+IHasFingas@users.noreply.github.com>
2025-06-06 13:40:38 +0200

    feat(class): Added Inventory class

---
+++ backend/ERPNumber1/ERPNumber1/Models/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ERPNumber1.Models
+{
+    public class Inventory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public List<Material> Materials { get; set; }
+
+        public Inventory(int Id, string Name, int Quantity, List<Material> Materials)
+        {
+            
+        }
+    }
+}
