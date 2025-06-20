85f0093879ec7ad072c86994f474a28a404b61ad
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2025-06-20 21:57:58 +0200

    Update variables.tf

--- infra/frontend/variables.tf
+++ infra/frontend/variables.tf
@@ -33,5 +33,5 @@ variable "admin_ssh_public_key" {
 variable "force_recreate" {
   description = "Force recreate the VM if updated"
   type        = string
-  default     = "2025-06-20.2"
+  default     = "2025-06-20.3"
 }
