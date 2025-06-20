67bc12cd0cf477ca065a6163cb7a79ae698bfb7b
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2025-06-20 18:33:46 +0200

    fix(infra-nsg): moved SSH allow to the correct location

--- infra/frontend/nsg.tf
+++ infra/frontend/nsg.tf
@@ -5,6 +5,20 @@ resource "azurerm_network_security_group" "frontend" {
 
 
   security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "86.82.163.85"
+    destination_address_prefix = "*"
+  }
+
+
+
+  security_rule {
     name                       = "AllowHTTPSAccess"
     description                = "Allow HTTPS access"
     priority                   = 1011
