bdb353b37ee9138ed0d30b05378a8cf87a2100c2
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2025-06-20 16:19:41 +0200

    fix(infra): removed inbound SQL rule from backend as it is not needed

--- infra/NSGBack.tf
+++ infra/NSGBack.tf
@@ -18,19 +18,20 @@ resource "azurerm_network_security_group" "NSG_Backend" {
   }
 
 
-  security_rule {
-    name                       = "MySQL-Accessin"
-    description                = "Allow MySQL access"
-    priority                   = 140
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3306"
-    source_address_prefix      = "10.3.1.10"
-    destination_address_prefix = "*"
+  # NOTE: Inbound rule for MySQL is not needed on the backend NSG, as it is handled by the database NSG.
+  # security_rule {
+  #   name                       = "MySQL-Accessin"
+  #   description                = "Allow MySQL access"
+  #   priority                   = 140
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "3306"
+  #   source_address_prefix      = "10.3.1.10"
+  #   destination_address_prefix = "*"
 
-  }
+  # }
 
 
   security_rule {
