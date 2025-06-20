b50ffe04ab9c75e2f6f941ccaa6a956894a83c98
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2025-06-20 16:01:25 +0200

    fix(infra): split up into multiple files

---
+++ infra/database.tf
@@ -0,0 +1,55 @@
+resource "azurerm_subnet" "database" {
+  name                 = "DatabaseSubnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+resource "azurerm_network_interface" "database" {
+  name                = "nic-linux-docker"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.frontend.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "database" {
+  name                            = "vm-linux-docker"
+  resource_group_name             = data.azurerm_resource_group.rg.name
+  location                        = data.azurerm_resource_group.rg.location
+  size                            = "Standard_B1s"
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  disable_password_authentication = true
+
+  network_interface_ids = [
+    azurerm_network_interface.database.id,
+  ]
+
+  os_disk {
+    name                 = "disk-linux-docker"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  computer_name      = "linuxdocker"
+  provision_vm_agent = true
+
+  custom_data = base64encode(file("cloud-init.yaml"))
+
+  tags = {
+    environment = "production"
+  }
+}
--- infra/network.tf
+++ infra/network.tf
@@ -1,27 +1,31 @@
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "vnet" {
   name                = "Simulatienetwerk"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 
-
-  subnet {
-    name             = "FrontendSubnet"
-    address_prefixes = ["10.0.1.0/24"]
-  }
-
-  subnet {
-    name             = "BackendSubnet"
-    address_prefixes = ["10.0.2.0/24"]
-
-  }
-
-  subnet {
-    name             = "DatabaseSubnet"
-    address_prefixes = ["10.0.3.0/24"]
-
-  }
-    tags = {
+  tags = {
     environment = "Production"
   }
+}
+
+resource "azurerm_subnet" "frontend" {
+  name                 = "FrontendSubnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "backend" {
+  name                 = "BackendSubnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "pip-linux-docker"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  allocation_method   = "Static"
 }
