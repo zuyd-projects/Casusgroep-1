4440e7efe6c82adaac65fc25cea0745d8284547f
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2025-06-20 11:05:26 +0200

    fix(infra): fixed RG references and filename

--- infra/NSGFront.tf
+++ infra/NSGFront.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_security_group" "NSG_Frontend" {
   name                = "NSGFrontend"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 
   security_rule {
     name                       = "deny-all-inbound"
--- infra/network.tf
+++ infra/network.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "example" {
   name                = "Simulatienetwerk"
-  location            = azurerm_resource_group.example.location
-    resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
 
 
