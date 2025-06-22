bcedf1f36296e48de97b16e196eab5de220b5f40
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2025-06-22 06:45:06 +0200

    feat(database): added database infrastructure

--- infra/database/main.tf
+++ infra/database/main.tf
@@ -9,3 +9,53 @@ resource "azurerm_subnet_network_security_group_association" "database" {
   subnet_id                 = azurerm_subnet.database.id
   network_security_group_id = azurerm_network_security_group.database.id
 }
+
+resource "azurerm_network_interface" "database" {
+  name                = "prod-database-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.database.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.3.10"
+    public_ip_address_id          = var.public_ip_id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "database" {
+  name                            = "prod-database-vm"
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  size                            = "Standard_B1s"
+  admin_username                  = "azureuser"
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.database.id,
+  ]
+
+  os_disk {
+    name                 = "prod-database-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  computer_name      = "database-vm"
+  provision_vm_agent = true
+
+  custom_data = var.cloud_init
+
+  tags = {
+    environment = "production"
+  }
+}
